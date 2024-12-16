babebb4b40f3753058cff5e4831e509a69601c8f
tukanaft <tukanaft@gmail.com>
2024-12-17 01:41:36 +0300

    checkstyle

--- server/src/main/java/ru/practicum/shareit/request/RequestController.java
+++ server/src/main/java/ru/practicum/shareit/request/RequestController.java
@@ -1,6 +1,5 @@
 package ru.practicum.shareit.request;
 
-import jakarta.persistence.criteria.CriteriaBuilder;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
